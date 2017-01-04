d1fd0708280b7047e849d25e1c3925cf9e871f4b
Franck Legros <franck.legros@transatel.com>
2017-01-04 18:15:46 +0100

    resteasy 2

---
+++ resteasy/src/main/java/resteasy/ExampleApplication.java
@@ -0,0 +1,12 @@
+package resteasy;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("rest")
+public class ExampleApplication extends Application{
+
+}
--- resteasy/src/main/java/resteasy/resources/HelloWorldService.java
+++ resteasy/src/main/java/resteasy/resources/HelloWorldService.java
@@ -1,20 +1,36 @@
 package resteasy.resources;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import java.util.List;
+
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
 @Path("/HelloWorld")
 public class HelloWorldService extends Application{
 
 
     public static final String HELLO_WORLD = "Hello World!";
+    public static final String BYE_WORLD = "Bye World!";
 
     @GET
     @Path("/sayHello")
+    @Produces(TEXT_PLAIN)
     public String sayHello()
     {
         return HELLO_WORLD;
     }
 
+    @POST
+    @Produces(TEXT_PLAIN)
+    public String orderResource() {
+
+        return BYE_WORLD;
+
+    }
 }
